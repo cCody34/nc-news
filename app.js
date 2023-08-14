392b35c4e56ea51d8aea3cccaef8b8ae0d9d0e97
“cCody34git config --global user.name cCody34git config -- global user.name cCody34 <charlotte.cody23@gmail.com>
2023-08-14 12:17:13 +0100

    update app to not include data

--- app.js
+++ app.js
@@ -1,10 +1,7 @@
 const express = require("express");
 const healthCheckController = require("./db/controllers/health-check-controller.js");
 const { getTopics } = require("./db/controllers/topics-controller");
-const data = require("./db/data/development-data");
-
 const app = express();
-app.use(express.json());
 
 app.get("/api/healthcheck", healthCheckController);
 
