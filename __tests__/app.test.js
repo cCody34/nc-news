5e6314d49fb71612596284d096c93a1a2b29f2f3
“cCody34git config --global user.name cCody34git config -- global user.name cCody34 <charlotte.cody23@gmail.com>
2023-08-14 11:46:35 +0100

    get topics 200

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,5 +1,16 @@
 const app = require("../app");
 const request = require("supertest");
+const seed = require("../db/seeds/seed");
+const data = require("../db/data/test-data");
+const connection = require("../db/connection");
+
+afterAll(() => {
+  return connection.end();
+});
+
+beforeEach(() => {
+  return seed(data);
+});
 
 describe("app", () => {
   describe("/api/healthCheck", () => {
@@ -13,6 +24,24 @@ describe("app", () => {
         .then((response) => {
           const { body } = response;
           expect(body).toHaveProperty("msg", "server is running");
+        });
+    });
+  });
+  describe("/api/topics", () => {
+    test("200: responds with a status of 200", () => {
+      return request(app).get("/api/topics").expect(200);
+    });
+    test("200: responds with topics array on the response body", () => {
+      return request(app)
+        .get("/api/topics")
+        .expect(200)
+        .then(({ body }) => {
+          const { topics } = body;
+          expect(topics).toHaveLength(3);
+          topics.forEach((topic) => {
+            expect(topic).toHaveProperty("slug", expect.any(String));
+            expect(topic).toHaveProperty("description", expect.any(String));
+          });
         });
     });
   });
---
+++ db/controllers/topics-controller.js
@@ -0,0 +1,7 @@
+const { readTopics } = require("../models/topics-model");
+
+exports.getTopics = (req, res, next) => {
+  readTopics().then((topics) => {
+    res.status(200).send({ topics });
+  });
+};
